7323224a69cb239ae3129b65de49f097ad8b897b
Amanda Romero <mandicap@comcast.net>
2025-05-18 14:53:10 -0600

    Styles the list of weather data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,15 +10,34 @@
     @livewireStyles
 </head>
 <body>
-    <x-nav-bar />
+<x-nav-bar/>
 
-    <div class="px-4 py-5 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="pb-5 border-b border-gray-200">
-            <h3 class="text-base font-semibold text-gray-900">Weather Dashboard</h3>
-        </div>
-        <livewire:weather-dashboard/>
+<div class="px-4 py-5 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="px-4 sm:px-0">
+        <h3 class="font-semibold text-gray-900 text-base/7">Weather Dashboard</h3>
     </div>
+    <div class="mt-6 border-t border-gray-100">
+        <dl class="divide-y divide-gray-100">
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt class="font-medium text-gray-900 text-sm/6">Current Temperature</dt>
+                <dd class="mt-1 text-gray-700 text-sm/6 sm:col-span-2 sm:mt-0">{{ $weather['current']['temperature_2m'] . $weather['current_units']['temperature_2m'] }}</dd>
+            </div>
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt class="font-medium text-gray-900 text-sm/6">Current Condition</dt>
+                <dd class="mt-1 text-gray-700 text-sm/6 sm:col-span-2 sm:mt-0">{{ $weather['current']['precipitation'] == 0 ? 'Sunny' : $weather['current']['precipitation'] . $weather['current_units']['precipitation'] . ' of precipitation' }}</dd>
+            </div>
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt class="font-medium text-gray-900 text-sm/6">Humidity</dt>
+                <dd class="mt-1 text-gray-700 text-sm/6 sm:col-span-2 sm:mt-0">{{ $weather['current']['relative_humidity_2m'] . $weather['current_units']['relative_humidity_2m'] }}</dd>
+            </div>
+            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                <dt class="font-medium text-gray-900 text-sm/6">Wind Speed</dt>
+                <dd class="mt-1 text-gray-700 text-sm/6 sm:col-span-2 sm:mt-0">{{ $weather['current']['wind_speed_10m'] . $weather['current_units']['wind_speed_10m'] }}</dd>
+            </div>
+        </dl>
+    </div>
+</div>
 
-    @livewireScripts
+@livewireScripts
 </body>
 </html>
